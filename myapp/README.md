8d68b614052e409b92a02dc6f45e67bce14bc3b2
Krishna <kpratapsingh026@gmail.com>
2024-05-27 18:10:43 +0530

    First Commit

---
+++ myapp/README.md
@@ -0,0 +1,23 @@
+### `Features` ###
+Click on the pentagon to change its color.
+
+The text color inside the pentagon changes to ensure readability with the background color.
+
+Toggle between gray and black background colors.
+### `File Structure` ### 
+
+src/ : Contains the source code of the application.
+
+App.js : Main component that renders the pentagon and handles color changes.
+
+App.css : Stylesheet for the application.
+
+README.md : This file, containing information about the project.
+
+### `Dependencies` ###
+
+React: A JavaScript library for building user interfaces.
+
+react-dom: A package for rendering React components in the DOM.
+
+react-scripts: Scripts and configurations used by Create React App.
---
+++ myapp/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [toggle, setToggle] = useState(true);
+
+    const handleClick = () => {
+        setToggle(!toggle);
+    };
+
+    return (
+        <>
+            <header>
+                <h1 className='alignCenter'>Impact Analytics Assignment</h1>
+            </header>
+            <section className='center'>
+                <h1 className='alignCenter'>Click on pentagon to change color</h1>
+                <div
+                    className={`pentagon ${toggle ? 'gray' : 'black'}`}
+                    onClick={handleClick}
+                >
+                    <h1>Pentagon</h1>
+                    <h3>Impact Analytics</h3>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default App;
